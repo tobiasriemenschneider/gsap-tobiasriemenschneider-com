937ff3516dfbf22409691eb99b5f8481bb27f120
Tobias Riemenschneider <t.riemenschneider@mac.com>
2022-04-21 11:45:17 +0200

    landing page

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,5 @@
-import type { MetaFunction } from "@remix-run/node";
+// imports - REMIX
+import type { MetaFunction, LinksFunction } from "@remix-run/node";
 import {
   Links,
   LiveReload,
@@ -7,13 +8,28 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+// imports - STYLES
+import stylesGlobal from "./styles/global.css";
+import stylesFonts from "./styles/font-faces.css";
+import stylesNormalize from "./styles/normalize.css";
 
+// * META
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "New Remix App",
+  title: "GSAP Sandbox | gsap.tobiasriemenschneider.com",
   viewport: "width=device-width,initial-scale=1",
 });
 
+// * LINKS
+export const links: LinksFunction = () => {
+  return [
+    { rel: "stylesheet", href: stylesGlobal },
+    { rel: "stylesheet", href: stylesFonts },
+    { rel: "stylesheet", href: stylesNormalize },
+  ];
+};
+
+// * DEFAULT EXPORT
 export default function App() {
   return (
     <html lang="en">
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,40 @@
+// imports - REMIX
+import type { MetaFunction, LinksFunction } from "@remix-run/node";
+// imports - STYLES
+import stylesLanding from "~/styles/landing.css";
+// imports - LOGO
+import Guy from "~/images/gsap-guy.svg";
+
+// * LINKS
+export const links: LinksFunction = () => {
+  return [{ rel: "stylesheet", href: stylesLanding }];
+};
+
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div className="container">
+      <div className="inner">
+        <div className="logo">
+          <img src={Guy} alt="GSAP Guy" />
+        </div>
+        <div className="prose">
+          <h1>GSAP&nbsp;Sandbox</h1>
+          <p>
+            You reached a satellite of{" "}
+            <a href="tobiasriemenschneider.com">tobiasriemenschneider.com</a>.
+          </p>
+          <p>
+            This specific one is used to play around with{" "}
+            <a href="https://greensock.com/gsap/">GSAP from Greensock</a>, the
+            professional-grade animation platform for the web..
+          </p>
+          <p>
+            Feel free to go{" "}
+            <a href="https://tobiasriemenschneider.com/gsap-sandbox">here</a> to
+            get some more info.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
